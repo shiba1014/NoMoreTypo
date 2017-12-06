d5fd9e7c1cf3e063bb3c495b82f284d6e3e5f57f
Satsuki <shiba.png@gmail.com>
2017-12-06 13:13:48 +0900

    Initial Commit

---
+++ NoMoreTypoForXcode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoMoreTypoForXcode
+//
+//  Created by 羽柴 彩月 on 2017/12/06.
+//  Copyright © 2017年 Satsuki Hashiba. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NoMoreTypoForXcode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoMoreTypoForXcode
+//
+//  Created by 羽柴 彩月 on 2017/12/06.
+//  Copyright © 2017年 Satsuki Hashiba. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
